3b898a0724041a256272060822c677b6a89ff6ce
sserrano1965 <salvador_serrano132@hotmail.com>
2025-07-28 19:51:04 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('languageSwitcher').addEventListener('change', function() {
-  const lang = this.value;
-  document.querySelectorAll('[data-en]').forEach(el => {
-    el.textContent = el.getAttribute('data-' + lang);
-  });
-});
